4140106119000eb2bd40a91d4672e478f6ca8156
JS999 <jamessmith99@gmail.com>
2017-07-21 14:35:55 +0100

    test

---
+++ ConsoleApplication1a/ConsoleApplication1a/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testing = 123;
+        }
+    }
+}
